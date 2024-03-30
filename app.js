cb28b0e8665bced934c3d59c2552c6e37f1007d0
Rajat Mittal <rajat.mittal@precisely.com>
2024-03-30 17:42:24 +0530

    environment_var_config added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+
+
+
 const userRoutes = require('./routes/user');
 const blogRoutes = require('./routes/blog');
 const Blog = require('./models/blog');
@@ -11,7 +16,7 @@ const { checkForAuthentication } = require('./middlewares/auth');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/blogs").then((e) => console.log("MongoDB Connected!"));
+mongoose.connect(process.env.MONGO_URL).then((e) => console.log("MongoDB Connected!"));
 
 
 app.use(express.urlencoded({ extended: true }));
